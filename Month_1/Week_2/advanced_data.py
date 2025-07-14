677d4cad6de0ff8aae8e6877e57c0f45bfff104c
Gagan N Bangaragiri <107536730+BurntDosa@users.noreply.github.com>
2025-07-14 08:52:42 +0530

    Nested Dictionaries

--- Month_1/Week_2/advanced_data.py
+++ Month_1/Week_2/advanced_data.py
@@ -1,3 +1,4 @@
+# Nested Lists and flattening
 ml_datasets = [
     ["Iris", "Boston Housing"],
     ["MNIST", "CIFAR-10"],
@@ -5,4 +6,25 @@ ml_datasets = [
 ]
 
 flattened = [dataset for group in ml_datasets for dataset in group]
-print(flattened)
+#print(flattened)
+
+model_config = {
+    'model_type': 'regression',
+    'params': {
+        'learning_rate': 0.01,
+        'epochs' : 100
+    },
+    'features': ['age', 'income']
+}
+
+model = input("Choose the model you want to use: ")
+print(model)
+
+if model == "Linear Regression":
+    model_config['model_type'] = model
+    model_config['params']['learning_rate'] = 0.005
+else:
+    model_config['params']['learning_rate'] = 0.1
+
+model_config['optimised_for'] = 'Mac ARM'
+print(model_config)
